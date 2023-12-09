3debf129233caef77b5dfa37209f72d4ef671cb2
jbualuang9179 <jbualuang21@gmail.com>
2023-12-09 13:49:07 +0700

    add file drink and main.java

--- com/tanntime/Drink.java
+++ com/tanntime/Drink.java
@@ -1,2 +1,45 @@
+package com.tanntime;
+
 public class Drink {
+    private String menu, category;
+    private int price;
+
+    public Drink(String menu, String category, int price) {
+        this.menu = menu;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "menu='" + menu + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- com/tanntime/Main.java
+++ com/tanntime/Main.java
@@ -1,2 +1,31 @@
-package com.tanntime;public class Main {
+package com.tanntime;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here.
+        demoArrayList1();
+    }
+
+    // ArrayList
+    public static void demoArrayList1() {
+        ArrayList<Drink> menus = new ArrayList<Drink>();
+        menus.add(new Drink("orange", "Juice", 40));
+        menus.add(new Drink("Apple", "Juice", 45));
+        menus.add(new Drink("lemon", "Juice", 50));
+        menus.add(new Drink("Coke", "Soda", 25));
+        menus.add(new Drink("Sprite", "Soda", 25));
+        menus.add(new Drink("Fanta", "Soda", 25));
+        menus.add(new Drink("Latte", "Coffee", 70));
+        menus.add(new Drink("Mocha", "Coffee", 80));
+        menus.add(new Drink("Cappuccino", "Coffee", 90));
+        menus.add(new Drink("Espresso", "Coffee", 65));
+
+        // iterate
+        for (Drink menu : menus) {
+            System.out.println(menu.toString());
+        }
+    }
 }
