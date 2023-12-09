8f9d23f1b876342c67f76681de423a4bd6653792
jbualuang9179 <jbualuang21@gmail.com>
2023-12-09 14:56:42 +0700

    no message

--- com/tanntime/Drink.java
+++ com/tanntime/Drink.java
@@ -1,8 +1,23 @@
 package com.tanntime;
 
-public class Drink {
+import java.util.Objects;
+
+public class Drink implements Comparable<Drink>{
     private String menu, category;
     private int price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Drink drink = (Drink) o;
+        return price == drink.price && Objects.equals(menu, drink.menu) && Objects.equals(category, drink.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menu, category, price);
+    }
 
     public Drink(String menu, String category, int price) {
         this.menu = menu;
@@ -41,5 +56,10 @@ public class Drink {
                 ", category='" + category + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Drink o) {
+        return 0;
     }
 }
--- com/tanntime/Main.java
+++ com/tanntime/Main.java
@@ -6,7 +6,45 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here.
-        demoArrayList1();
+//        demoArrayList1();
+        demoSimpleArrayList();
+    }
+
+
+//    public static void advArrayList() {
+//        ArrayList<Drink> menus = createDrinkData();
+//        for (Drink menu : menus) {
+//            System.out.println(menu.toString());
+//        }
+//
+//        // create instance
+//        Drink m = new Drink("lemon", "Juice", 50);
+//        System.out.println(menus.contains(m));
+//
+//
+//    }
+
+    public static void demoSimpleArrayList() {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("banana"); // 0
+        fruits.add("orange"); // 1
+        fruits.add("kiwi"); // 2
+
+        System.out.println(fruits.get(1));
+        System.out.println(fruits.indexOf("orange"));
+        System.out.println(fruits.contains("kiwi"));
+
+        // add value into array
+        fruits.add(1, "coconut");
+        System.out.println(fruits.toString());
+        System.out.println(fruits.get(2));
+
+        fruits.add(0, "apple");
+        System.out.println(fruits.toString());
+
+        // edit value
+        fruits.set(1, "mango");
+        System.out.println(fruits.toString());
     }
 
     // ArrayList
