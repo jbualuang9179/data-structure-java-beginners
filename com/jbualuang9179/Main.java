aa86df655615f9f4cb52d9b2f03d8b749c21d386
Jirapong Bualuang <jbualuang21@gmail.com>
2025-07-05 20:58:22 +0700

    init java collection

---
+++ com/jbualuang9179/Main.java
@@ -0,0 +1,10 @@
+package com.jbualuang9179;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dsa");
+
+        // Introduction of Searching [Linear Search & Binary Search]
+
+    }
+}
---
+++ com/jbualuang9179/collection/Main.java
@@ -0,0 +1,9 @@
+package com.jbualuang9179.collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Need for a Separate Collection Framework in Java
+
+    }
+}
